1cfed19ab7d01384e3abfba672d272e735c795e5
Roya982 <roayarana@gmail.com>
2021-02-10 15:51:54 +0200

    assigment edition by Roya

--- js/main.js
+++ js/main.js
@@ -2,55 +2,69 @@
 
 
 let userName = prompt('please inter your name', "<name gose her>");
+function yourName(userName){
 alert('welcome to become friends.. ' + userName);
 console.log(userName);
+}
 
 let me = prompt('Im I 26 years old ?', "<yes or no>");
 
-if (me.toLowerCase === 'yes') {
+function myAge(me){if (me.toLowerCase === 'yes') {
     alert('it\'s true');
     //console.log('correct')
 } else if (me.toLowerCase() === 'no') {
     alert('it\'s 26');
 }
+}
 
 let back = prompt(' dose my back ground about interior design?', '<yes or no>');
 
-if (back.toLowerCase() === 'yes') {
+function babyBack(back){ if (back.toLowerCase() === 'yes') {
     alert('true');
     //console.log('correct')
 } else if (back.toLowerCase() === 'no') {
     alert('false');
 }
+}
+
 
 let challenges = prompt('dose I accept challenges ?', '<yes or no>');
 
-if (challenges.toLowerCase() === 'no') {
+function loveChallenges(challenges){ if (challenges.toLowerCase() === 'no') {
     alert('yes I does');
     //console.log('correct')
 } else if (challenges.toLowerCase() === 'yes') {
     alert(' we start be come friends');
 }
+}
+
 
 let ask = prompt('do you will ask me to draw you?', '<yes or no>');
 
-if (ask.toLowerCase() === 'yes') {
+function meAske(ask){ if (ask.toLowerCase() === 'yes') {
 
     alert('do not even try');
 } else if (ask.toLowerCase() === 'no') {
     alert('great I like you ');
     //console.log('correct')
 }
-
+}
 let know = prompt(' did you start to know me?', '<yes or no>');
 
-if (know.toLowerCase() === 'yes') {
+function youKnow (know){ if (know.toLowerCase() === 'yes') {
     alert('great read about me to know more');
     //console.log('correct')
 } else if (know.toLowerCase() === 'no') {
     alert(' here you can read about me to know me more.');
     //console.log('correct')
 }
+}
+yourName(userName);
+myAge(me);
+babyBack(back);
+loveChallenges(challenges);
+meAsk(ask);
+youKnow(know);
 
 alert(userName + (' ..hello agin'))
 console.log(userName, know, ask, me, challenges, back)
